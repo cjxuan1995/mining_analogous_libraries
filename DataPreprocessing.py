51fb90db40ae3dab9ce68feba1198e480d8c09e3
RobertCao <cjxuan1995@outlook.com>
2017-03-30 09:10:42 +1100

    first code refactoring

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,61 +1,58 @@
 import re
 import MySQLdb
 
-db = MySQLdb.connect(host = '', user = '', passwd='', db = '') #Fill in details about your MySQL database
-file1 = open("raw_data.txt","w")
-file2 = open("processed_data.txt","w")
-i = 0
-cursor = db.cursor()
-cursor.execute("") #This is where you execute your MySQL query
-rows = [item[0] for item in cursor.fetchall()]
+def getDataFromDB():
+    host = raw_input("host: ")
+    user = raw_input("user: ")
+    password = raw_input("password: ")
+    database = raw_input("database: ")
+    db = MySQLdb.connect(host, user, password, database)
+    cursor = db.cursor()
+    cursor.execute("select Body from posts limit 1000")
+    rows = [item[0] for item in cursor.fetchall()]
+    db.close()
+    return rows
 
+def processDataRegex(data):
+    symbolList = ['=', '*', '{', '}', '[', ']', '&', '$']
+    pattern = re.sub("<code>(.*?)</code>",
+                     lambda m: "" if any(symbol in m.group(1) for symbol in symbolList) else m.group(), data,
+                     flags=re.S)
 
-for row in rows:
-    sentence_list = []
-    wordList = []
-    data = str(row)
-
-    #This block below is for determining what is inside <code> and </code>. Only keep those pure text content.
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "=" in m.group(1) else m.group(), data, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "+" in m.group(1) else m.group(), pattern, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "*" in m.group(1) else m.group(), pattern, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "(" in m.group(1) else m.group(), pattern, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "{" in m.group(1) else m.group(), pattern, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "&" in m.group(1) else m.group(), pattern, flags=re.S)
-    pattern = re.sub("<code>(.*?)</code>", lambda m: "" if "-" in m.group(1) else m.group(), pattern, flags=re.S)
-
-    #The block below is for removing html code and other symbols in the data to get more readable data
     pattern = re.sub("</?[a-z][^>]*>", "", pattern)
-    pattern = re.sub("</?blockquote>","",pattern)
-    pattern = re.sub("&#xA;"," ", pattern)
-    pattern = re.sub("&nbsp;","",pattern)
-    pattern = re.sub("\*","",pattern)
-    pattern = re.sub("&", "", pattern)
-    pattern = re.sub("~", "", pattern)
-    pattern = re.sub("`", "", pattern)
-    pattern = re.sub("http.*? ", " ", pattern)
-    pattern = re.sub("http.*?$", "", pattern)
+    pattern = re.sub("&nbsp;|&lt;|&gt;|&amp;|&quot;|&apos;", "", pattern)
+    pattern = re.sub("\*|~|`", "", pattern)
 
-    pattern = pattern.lower()#lowercase all the letters in the data
-    sentence_list = re.split("&#xA;|!|\?|;|\. ", pattern)#split the data into sentences
+    pattern = re.sub("http.+?&#xA", "&#xA", pattern)
+    pattern = re.sub("http.+? ", " ", pattern)
+    pattern = re.sub("http.+?$", "", pattern)
+    pattern = pattern.lower()
+    return pattern
+
+def separateSentAndWord(processedData):
+    wordList = []
+    sentence_list = re.split("&#xa;|!|\?|;|\. ", processedData)
 
     for sentence in sentence_list:
         if (not sentence.isspace()) and sentence:
-            wordList.append(re.sub("[^\w]", " ", sentence).split()) #split each sentence to words
+            sentence = re.sub("\.$", "", sentence)
+            wordList.append(re.sub("[^\w|+|.|#|-]", " ", sentence).split())
 
-    for words in wordList:
-        for i, word in enumerate(words):
-            if (i + 1) == len(words):
-                file2.write("%s\n" % word) #write the list of words into a file named "processed_data". Each row in the file contains words of a sentence, and those words are separated by space.
-            else:
-                file2.write("%s" % word + " ")
+    return wordList
 
 
-    file1.write(str(i) + ".%s\n" % data) #write raw data into a file named "raw_data"
+def main():
+    rows = getDataFromDB()
+    fileName = raw_input("The name of the file storing processed data: ")
+    file = open(fileName, "w")
+    for row in rows:
+        processedData = processDataRegex(str(row))
+        wordList = separateSentAndWord(processedData)
 
+        for words in wordList:
+            newStr = " "
+            file.write("%s\n" % newStr.join(words))
 
-file1.close()
-file2.close()
+    file.close()
 
-
-db.close()
+if __name__ == "__main__": main()
