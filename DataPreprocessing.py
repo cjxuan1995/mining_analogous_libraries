a5f8968fbabd0e5b6c6f72eade5de068afff9a3b
RobertCao <cjxuan1995@outlook.com>
2017-04-07 20:18:00 +1000

    fixed indent issue

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -3,14 +3,14 @@ import MySQLdb
 
 
 class Processor(object):
-    """The Processor class processes the raw stack_overflow post data to more readable form.
-
+    """The Processor class process the raw stack_overflow post data to more readable form.
+    
     It contains five methods. Basically, only three methods should be accessed externally, which are get_data_from_db, write_data_to_file and write_raw_and_processed_data_to_file.
     """
 
     def get_data_from_db(self, host, user, password, database, command):
         """Retrieve data from a table in a MySQL database
-
+        
         :param host: host name of your database
         :param user: user of your database
         :param password: password for your database
@@ -27,17 +27,17 @@ class Processor(object):
 
     def __process_data_regex(self, data):
         """Use regular expression to clean the raw data to get more readable data
-
+        
         :param data: strings, which are converted from rows retrieved from the database
         :return: strings, which are the processed data
         """
-        symbol_list = ['=', '*', '{', '}', '[', ']', '&', '$']  #This line and the next line mean that if any of those symbols appear in the code block, it will be considered as code, which will be removed.
+        symbol_list = ['=', '*', '{', '}', '[', ']', '&', '$']
         pattern = re.sub("<code>(.*?)</code>",
                          lambda m: "" if any(symbol in m.group(1) for symbol in symbol_list) else m.group(), data,
                          flags=re.S)
 
         pattern = re.sub(r"</?[a-z][^>]*>", " ", pattern)
-        pattern = re.sub(r"&nbsp;|&lt;|&gt;|&amp;|&quot;|&apos;|e\.g\.|i\.e\.", " ", pattern)  #remove some html entities and special expressions
+        pattern = re.sub(r"&nbsp;|&lt;|&gt;|&amp;|&quot;|&apos;|e\.g\.|i\.e\.", " ", pattern)
         pattern = re.sub(r"\*|~|`", " ", pattern)
         pattern = re.sub(r"&#xA", ". ", pattern)
 
@@ -49,7 +49,7 @@ class Processor(object):
 
     def __separate_sentence_and_word(self, processed_data):
         """Separate sentences and words in the processed data
-
+        
         :param processed_data: strings, which are the output of __process_data_regrex
         :return: a two-dimensional list which contains all the words in the processed data.
                  For example, [[i, like, football][this, python, module, is, very, confusing]]
@@ -60,16 +60,16 @@ class Processor(object):
         for sentence in sentence_list:
             if (not sentence.isspace()) and sentence:
                 sentence = re.sub(r"\.$", "", sentence)
-                word_list.append(re.sub(r"[^\w|\+|\.|#|-]", " ", sentence).split())  #Keep letters, numbers, +, ., # and -. Otherwise, replace them with space. The reason for keeping +, ., # is because they may be part of some software terms. C++, C#, angular.js and so on.
+                word_list.append(re.sub(r"[^\w|\+|\.|#|-]", " ", sentence).split())
 
-    return wordList
+        return word_list
 
     def write_data_to_file(self, file_name, rows):
         """write processed data into a file
-
-        :param file_name: name of the file you want to store the processed data
+        
+        :param file_name: name of the file you want to store the processed data 
         :param rows: output of get_data_from_db, which are rows obtains from database
-        :return:
+        :return: 
         """
         with open(file_name, 'w') as data_file:
             for row in rows:
@@ -83,10 +83,10 @@ class Processor(object):
 
     def write_raw_and_processed_data_to_file(self, file_name, rows):
         """write both raw data and processed data into a file
-
-        :param file_name: name of the file you want to store the processed data
+        
+        :param file_name: name of the file you want to store the processed data 
         :param rows: output of get_data_from_db, which are rows obtains from database
-        :return:
+        :return: 
         """
         with open(file_name, 'w') as data_file:
             for row in rows:
@@ -100,3 +100,7 @@ class Processor(object):
                     data_file.write("%s\n" % new_str.join(words))
 
                 data_file.write("\n\n\n")
+
+
+
+
